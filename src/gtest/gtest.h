d53ae211e4f608814717b404917aeca9fd26cae8
Pannous <info@pannous.com>
2015-01-16 17:37:21 +0100

    ''

--- src/gtest/gtest.h
+++ src/gtest/gtest.h
@@ -47,7 +47,7 @@
 // Acknowledgment: Google Test borrowed the idea of automatic test
 // registration from Barthelemy Dagenais' (barthelemy@prologique.com)
 // easyUnit framework.
-
+#define GTEST_USE_OWN_TR1_TUPLE 1
 #ifndef GTEST_INCLUDE_GTEST_GTEST_H_
 #define GTEST_INCLUDE_GTEST_GTEST_H_
 
