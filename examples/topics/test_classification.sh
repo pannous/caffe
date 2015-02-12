0fce8d4e25ee49b809f1ad7cb758382a96832f0e
Pannous <info@pannous.com>
2015-02-12 15:11:52 +0100

    topics v0.1

---
+++ examples/topics/test_classification.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# I like to work in the example directory, not in root
+[ -d examples/classification ] && cd examples/classification
+
+# The only data file we use for training here:
+echo 'train_categories.h5' > filelist.txt
+
+# solver.prototxt links to network xor_net.prototxt
+# xor_net.prototxt specifies training filelist xor_data.filelist.txt
+# TODO training files should NOT be part of the NET definition!!
+
+# Make sure caffe is found
+type caffe || PATH=$PATH:../../build/tools/
+
+caffe train -solver=solver.prototxt
+
+# For a discussion of the xor net see here:
+# See https://groups.google.com/forum/?utm_medium=email&utm_source=footer#!msg/caffe-users/4KnV4BOhI60/7C5d7FqNIioJ
