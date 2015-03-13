7e0180b024a42580bf9611f18606b4ed17c26e40
Pannous <info@pannous.com>
2015-03-13 10:01:56 +0100

    prelu

--- src/caffe/layers/base_data_layer.cpp
+++ src/caffe/layers/base_data_layer.cpp
@@ -61,20 +61,20 @@ void BasePrefetchingDataLayer<Dtype>::Forward_cpu(
     const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
   // First, join the thread
   JoinPrefetchThread();
-  DLOG(INFO) << "Thread joined";
+  // DLOG(INFO) << "Thread joined";
   // Reshape to loaded data.
   top[0]->Reshape(this->prefetch_data_.num(), this->prefetch_data_.channels(),
       this->prefetch_data_.height(), this->prefetch_data_.width());
   // Copy the data
   caffe_copy(prefetch_data_.count(), prefetch_data_.cpu_data(),
              top[0]->mutable_cpu_data());
-  DLOG(INFO) << "Prefetch copied";
+  // DLOG(INFO) << "Prefetch copied";
   if (this->output_labels_) {
     caffe_copy(prefetch_label_.count(), prefetch_label_.cpu_data(),
                top[1]->mutable_cpu_data());
   }
   // Start a new prefetch thread
-  DLOG(INFO) << "CreatePrefetchThread";
+  // DLOG(INFO) << "CreatePrefetchThread";
   CreatePrefetchThread();
 }
 
